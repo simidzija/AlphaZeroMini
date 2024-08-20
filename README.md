a511901e84311069ba5b9951d92d2ce265490017
Petar Simidzija <psimidzija@gmail.com>
2024-08-20 16:41:08 -0700

    Added README, changed checkers.py to chess.py

---
+++ README.md
@@ -0,0 +1,4 @@
+## Two Kings
+
+A black king and a white king are initialized on opposite ends of a 5 by 5 board. They move like regular kings in chess, except they are allowed to come next to and take each other. The player who takes his opponents king wins the game. If no one wins after 20 moves the game is a draw. Perfect play results in a draw.
+
---
+++ chess.py
@@ -0,0 +1,28 @@
+import torch
+
+
+# Valid actions mask: input features -> mask containing valid actions
+
+
+
+# Policy and value network: input features -> prob dist over valid actions 
+
+
+
+# Implement GUI which lets me play :)
+
+
+
+# MCTS (think about how to structure this)
+
+
+
+# Training function
+
+
+
+# Save final model to disk
+
+
+
+# Load and play trained model
