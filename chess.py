e8bfdb8ac75777141bcbd8634f847d207b1997e1
Petar Simidzija <psimidzija@gmail.com>
2024-08-21 16:37:06 -0700

    Implemented Network class

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 
 # Valid actions mask: input features -> mask containing valid actions
@@ -26,6 +27,128 @@ def action_mask(features: torch.BoolTensor) -> torch.BoolTensor:
 
 
 # Policy and value network: input features -> prob dist over valid actions 
+class ResBlock(nn.Module):
+    """Two convolution layers with a residual skip connection."""
+    def __init__(self, num_filters: int, kernel_size: int):
+        super().__init__()
+        assert kernel_size % 2 == 1, f'kernel size must be odd, got {kernel_size}'
+        padding = kernel_size // 2
+        self.conv1 = nn.Conv2d(
+            in_channels=num_filters,
+            out_channels=num_filters,
+            kernel_size=kernel_size,
+            padding=padding
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=num_filters,
+            out_channels=num_filters,
+            kernel_size=kernel_size,
+            padding=padding
+        )
+        self.bn1 = nn.BatchNorm2d(num_features=num_filters)
+        self.bn2 = nn.BatchNorm2d(num_features=num_filters)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.bn1(y)
+        y = self.relu(y)
+        y = self.conv2(y)
+        y = self.bn2(y)
+        y = x + y
+        y = self.relu(y)
+
+        return y
+    
+class PolicyHead(nn.Module):
+    """Policy head."""
+    def __init__(self, board_size: int, num_filters:int, num_policy_filters: int, num_out_channels: int):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=num_filters, 
+                      out_channels=num_policy_filters, 
+                      kernel_size=1),
+            nn.BatchNorm2d(num_features=num_policy_filters),
+            nn.ReLU(),
+            nn.Flatten(1),
+            nn.Linear(in_features=num_policy_filters * board_size ** 2,
+                      out_features=num_out_channels * board_size ** 2),
+            nn.Unflatten(1, (num_out_channels, board_size, board_size))
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class ValueHead(nn.Module):
+    """Value Head."""
+    def __init__(self, board_size: int, num_filters: int, hidden_layer_size: int):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=num_filters, out_channels=1, kernel_size=1),
+            nn.BatchNorm2d(num_features=1),
+            nn.ReLU(),
+            nn.Flatten(1),
+            nn.Linear(in_features=board_size**2, 
+                      out_features=hidden_layer_size),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_layer_size, out_features=1),
+            nn.Tanh(),
+            nn.Flatten(0)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class Network(nn.Module):
+    """Policy and value network."""
+    def __init__(self, *, 
+                 in_channels: int, 
+                 board_size: int,
+                 num_filters: int, 
+                 kernel_size:int, 
+                 num_res_blocks: int,
+                 num_policy_filters: int,
+                 num_out_channels: int,
+                 value_hidden_layer_size: int
+                 ):
+        super().__init__()
+
+        self.block1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=num_filters,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2
+            ),
+            nn.BatchNorm2d(num_features=num_filters),
+            nn.ReLU()
+        )
+
+        self.res_blocks = nn.ModuleList([ResBlock(num_filters, kernel_size) for _ in range(num_res_blocks)])
+
+        self.policy_head = PolicyHead(
+            board_size=board_size,
+            num_filters=num_filters,
+            num_policy_filters=num_policy_filters,
+            num_out_channels=num_out_channels
+        )
+
+        self.value_head = ValueHead(
+            board_size=board_size,
+            num_filters=num_filters,
+            hidden_layer_size=value_hidden_layer_size
+        )
+
+
+    def forward(self, x):
+        y = self.block1(x)
+        for block in self.res_blocks:
+            y = block(y)
+        
+        policy = self.policy_head(y)
+        value = self.value_head(y)
+
+        return policy, value
 
 
 
--- test_chess.py
+++ test_chess.py
@@ -31,3 +31,82 @@ def test_action_mask():
     assert torch.all(mask_idxs == expected_idxs)
 
     
+def test_ResBlock():
+    batch_size = 16
+    num_filters = 8
+    board_size = 5
+    kernel_size = 3
+
+    block = chess.ResBlock(
+        num_filters=num_filters,
+        kernel_size=kernel_size
+    )
+
+    x = torch.rand(batch_size, num_filters, board_size, board_size)
+    y = block(x)
+
+    assert y.shape == x.shape
+
+
+def test_PolicyHead():
+    batch_size = 16
+    board_size = 5
+    num_filters = 8
+    num_policy_filters = 2
+    num_out_channels = 4
+
+    policy = chess.PolicyHead(
+        board_size=board_size,
+        num_filters=num_filters,
+        num_policy_filters=num_policy_filters,
+        num_out_channels=num_out_channels
+    )
+
+    x = torch.rand(batch_size, num_filters, board_size, board_size)
+    y = policy(x)
+
+    assert y.shape == torch.Size([batch_size, num_out_channels, board_size, board_size])
+
+
+def test_ValueHead():
+    batch_size = 16
+    board_size = 5
+    num_filters = 8
+    hidden_layer_size = 64
+
+    value = chess.ValueHead(
+        board_size=board_size,
+        num_filters=num_filters,
+        hidden_layer_size=hidden_layer_size
+    )
+
+    x = torch.rand(batch_size, num_filters, board_size, board_size)
+    y = value(x)
+
+    assert y.shape == torch.Size([batch_size])
+
+
+def test_Network():
+
+    batch_size = 16
+    params = {
+        'in_channels': 4, 
+        'board_size': 5,
+        'num_filters': 8, 
+        'kernel_size': 3, 
+        'num_res_blocks': 6,
+        'num_policy_filters': 2,
+        'num_out_channels': 4,
+        'value_hidden_layer_size': 64
+    }
+
+    network = chess.Network(**params)
+
+    x = torch.rand(batch_size, params['in_channels'], 
+                   params['board_size'], params['board_size'])
+    
+    action_dist, value = network(x)
+
+    assert action_dist.shape == torch.Size([batch_size, params['num_out_channels'], params['board_size'], params['board_size']])
+
+    assert value.shape == torch.Size([batch_size])
